d1e26195a6489713b13a237573313e195367ad16
Cansin Yildiz <cansin@udemy.com>
2017-11-14 17:38:30 -0800

    Move more Website ESLint config to eslint-config-udemy-website.

--- packages/eslint-config-udemy-website/index.js
+++ packages/eslint-config-udemy-website/index.js
@@ -9,8 +9,16 @@ module.exports = {
         'filenames',
         'underscore',
     ],
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: 'webpack/config-dev-without-webpack-dev-server.js',
+            },
+        },
+    },
     rules: {
         'filenames/match-regex': ['error', '^(?:\\.eslintrc|(?:[a-z0-9\\-]+(?:\\.(?:jqui-widget|ng-(?:constant|controller|directive|factory|filter|provider|service)|react-(?:component|proptypes)|mobx-(?:model|store)))?(?:\\.spec)?))$'],
+        'udemy/angular-path-based-module-names': ['error', 'src/udemy/js'],
         'underscore/prefer-noop': ['error', 'always'],
     },
     overrides: [
