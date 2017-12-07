915f7d28a1b690ac1d82e020f531a4a8d3f2d6e4
Cansin Yildiz <cansin@udemy.com>
2017-12-07 12:04:10 -0800

    Add docstring about the nature of text tested.

--- packages/eslint-config-udemy-babel-addons/tests.js
+++ packages/eslint-config-udemy-babel-addons/tests.js
@@ -5,5 +5,6 @@ const path = require('path');
 
 eslintConfigTester.runOnText(
     path.resolve(__dirname, 'index.js'),
+    // This is a minimal JS code, just to make sure config has no errors.
     "const hello = 'world';\nexport default hello;\n"
 );
