edd207862561ba866a6d74148947d2a04305faf8
Cansin Yildiz <cansin@udemy.com>
2017-12-06 18:58:34 -0800

    test: Add missing config tests

--- packages/eslint-config-udemy-jasmine-addons/tests.js
+++ packages/eslint-config-udemy-jasmine-addons/tests.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// noop
+const eslintConfigTester = require('eslint-config-tester');
+const path = require('path');
+
+eslintConfigTester.runOnText(
+    path.resolve(__dirname, 'index.js'),
+    "'use strict';\nmodule.exports = { hello: 'world' };\n"
+);
--- packages/eslint-config-udemy-website/tests.js
+++ packages/eslint-config-udemy-website/tests.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// noop
+const eslintConfigTester = require('eslint-config-tester');
+const path = require('path');
+
+eslintConfigTester.runOnText(
+    path.resolve(__dirname, 'index.js'),
+    "const hello = 'world';\nexport default hello;\n"
+);
