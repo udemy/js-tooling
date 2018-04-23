875c66dcaa93c151218513007d5bfb7afe84a065
kevinzang <kevin.zhang@udemy.com>
2018-04-23 12:22:42 -0700

    test: simplify the test cases

--- packages/eslint-plugin-udemy/rules/import-blacklist/tests.js
+++ packages/eslint-plugin-udemy/rules/import-blacklist/tests.js
@@ -13,136 +13,77 @@ const ruleTester = new RuleTester({
 
 const options = [[
     {
-        // For correct configuration
-        pattern: '^currencyformatter(?:\\.js)?$',
-        message: 'Please import from utils/currency-formatter/, not from currencyformatter',
-        exceptions: ['utils/currency-formatter(?:\\.spec)?\\.js$'],
+        pattern: '^apple(?:\\.js)?$',
+        message: 'Please import from fruit/apple/, not from apple',
+        exceptions: ['fruit/apple(?:\\.spec)?\\.js$'],
     },
     {
-        // For improved JS bundling
-        pattern: '^lodash(?:\\.js)?$',
-        message: 'Please import from e.g. lodash/foo, not from lodash directly',
+        pattern: '^grape/lib/grape(?:\\.js)?$',
+        message: 'Please import from grape, not from grape/lib/grape',
     },
     {
-        // For code consistency
-        pattern: '^mobx/lib/mobx(?:\\.js)?$',
-        message: 'Please import from mobx, not from mobx/lib/mobx',
+        pattern: '^lemon(?:\\.js)?$',
+        message: 'Please import from e.g. lemon/lib/foo, not from lemon directly',
     },
     {
-        // For correct configuration
-        pattern: '^moment(?:\\.js)?$',
-        message: 'Please import from utils/ud-moment, not from moment',
-        exceptions: ['utils/ud-moment(?:\\.spec)?\\.js$'],
-    },
-    {
-        // For correct configuration
-        pattern: '^numeral(?:\\.js)?$',
-        message: 'Please import from utils/ud-numeral, not from numeral',
-        exceptions: ['utils/ud-numeral(?:\\.spec)?\\.js$'],
-    },
-    {
-        // For improved JS bundling
-        pattern: '^react-bootstrap(?:\\.js)?$',
-        message: 'Please import from e.g. react-bootstrap/lib/foo, not from react-bootstrap directly',
-    },
-    {
-        // For correct implementation
-        pattern: '^react-bootstrap.*?$',
-        message: 'Please import from base-components/, not from react-bootstrap/',
-        exceptions: ['/base-components/'],
-    },
-    {
-        // For improved JS bundling
-        pattern: '^react-overlays(?:\\.js)?$',
-        message: 'Please import from e.g. react-overlays/lib/foo, not from react-overlays directly',
+        pattern: '^lemon.*?$',
+        message: 'Please import from lime/, not from lemon/',
+        exceptions: ['/lime/'],
     },
 ]];
 
 ruleTester.run('import-blacklist', rule, {
     valid: [
         {
-            code: "import currencyFormatter from 'currencyformatter.js';",
-            filename: path.join(__dirname, 'utils/currency-formatter.js'),
+            code: "import apple from 'apple.js';",
+            filename: path.join(__dirname, 'fruit/apple.js'),
             options,
         },
         {
-            code: "import example from 'lodash/example';",
+            code: "import apple from 'apple';",
+            filename: path.join(__dirname, 'fruit/apple.js'),
+            options,
+        },
+        {
+            code: "import grape from 'grape';",
             filename: path.join(__dirname, 'example.js'),
             options,
         },
         {
-            code: "import mobx from 'mobx';",
-            filename: path.join(__dirname, 'example.mobx-store.js'),
-            options,
-        },
-        {
-            code: "import moment from 'moment';",
-            filename: path.join(__dirname, 'utils/ud-moment.js'),
-            options,
-        },
-        {
-            code: "import numeral from 'numeral';",
-            filename: path.join(__dirname, 'utils/ud-numeral.js'),
-            options,
-        },
-        {
-            code: "import Example from 'react-bootstrap/lib/Example';",
-            filename: path.join(__dirname, 'base-components/example.react-component.js'),
-            options,
-        },
-        {
-            code: "import { Example } from 'react-overlays/lib/Example';",
-            filename: path.join(__dirname, 'example.react-component.js'),
+            code: "import foo from 'lemon/lib/foo';",
+            filename: path.join(__dirname, 'lime/example.js'),
             options,
         },
     ],
     invalid: [
         {
-            code: "import currencyFormatter from 'currencyformatter.js';",
-            errors: [{ message: 'Please import from utils/currency-formatter/, not from currencyformatter' }],
+            code: "import apple from 'apple.js';",
+            errors: [{ message: 'Please import from fruit/apple/, not from apple' }],
             filename: path.join(__dirname, 'example.js'),
             options,
         },
         {
-            code: "import { example } from 'lodash';",
-            errors: [{ message: 'Please import from e.g. lodash/foo, not from lodash directly' }],
+            code: "import apple from 'apple';",
+            errors: [{ message: 'Please import from fruit/apple/, not from apple' }],
             filename: path.join(__dirname, 'example.js'),
             options,
         },
         {
-            code: "import mobx from 'mobx/lib/mobx';",
-            errors: [{ message: 'Please import from mobx, not from mobx/lib/mobx' }],
-            filename: path.join(__dirname, 'example.mobx-store.js'),
-            options,
-        },
-        {
-            code: "import moment from 'moment';",
-            errors: [{ message: 'Please import from utils/ud-moment, not from moment' }],
+            code: "import grape from 'grape/lib/grape';",
+            errors: [{ message: 'Please import from grape, not from grape/lib/grape' }],
             filename: path.join(__dirname, 'example.js'),
             options,
         },
         {
-            code: "import numeral from 'numeral';",
-            errors: [{ message: 'Please import from utils/ud-numeral, not from numeral' }],
+            code: "import { foo } from 'lemon';",
+            errors: [{ message: 'Please import from e.g. lemon/lib/foo, not from lemon directly' }],
+            filename: path.join(__dirname, 'lime/example.js'),
+            options,
+        },
+        {
+            code: "import foo from 'lemon/lib/foo';",
+            errors: [{ message: 'Please import from lime/, not from lemon/' }],
             filename: path.join(__dirname, 'example.js'),
-            options,
-        },
-        {
-            code: "import { Example } from 'react-bootstrap';",
-            errors: [{ message: 'Please import from e.g. react-bootstrap/lib/foo, not from react-bootstrap directly' }],
-            filename: path.join(__dirname, 'base-components/example.react-component.js'),
-            options,
-        },
-        {
-            code: "import Example from 'react-bootstrap/lib/Example';",
-            errors: [{ message: 'Please import from base-components/, not from react-bootstrap/' }],
-            filename: path.join(__dirname, 'example.react-component.js'),
-            options,
-        },
-        {
-            code: "import { Example } from 'react-overlays';",
-            errors: [{ message: 'Please import from e.g. react-overlays/lib/foo, not from react-overlays directly' }],
-            filename: path.join(__dirname, 'example.react-component.js'),
             options,
         },
     ],
