83a63dd0f545460bdcbd3d00381b6888c12abaf1
JJ Behrens <jj.behrens@udemy.com>
2018-04-26 10:50:32 -0700

    Style

--- packages/babel-preset-udemy-website/index.js
+++ packages/babel-preset-udemy-website/index.js
@@ -7,9 +7,8 @@ module.exports = {
         const name = it[0];
         const options = it[1] || {};
         // eslint-disable-next-line import/no-dynamic-require
-        let plugin = require(`babel-plugin-${name}`);
+        const plugin = require(`babel-plugin-${name}`);
         // https://github.com/loganfsmyth/babel-plugin-transform-builtin-extend/issues/1
-        plugin = plugin.default || plugin;
-        return [plugin, options];
+        return [plugin.default || plugin, options];
     }),
 };
