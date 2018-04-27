2f8e386eb79590508563b739a1b3ce76b9d4305d
JJ Behrens <jj.behrens@udemy.com>
2018-04-27 13:33:54 -0700

    feat: Ignore *.global.less files

--- packages/babel-preset-udemy-website/tests/source/css-modules.js
+++ packages/babel-preset-udemy-website/tests/source/css-modules.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
-import './css-modules.less';
+import baseStyles from './css-modules.global.less';
+import styles from './css-modules.less';
 
 export default class CssModules extends Component {
     render() {
-        return <div styleName="greeting">Hello World!</div>;
+        // styles.greeting can be evaluated at compile time.
+        // baseStyles.colorful won't actually work at runtime since the *.global.less file is
+        // ignored, but I'm testing that it doesn't try to actually output a lookup table
+        // for baseStyles as it will for styles since I'm doing a dynamic styleName.
+        return (
+            <div className="colorful" styleName="styles.greeting">
+                <span styleName={ 'baseStyles.colorful' }>
+                    Hello World!
+                </span>
+            </div>
+        );
     }
 }
