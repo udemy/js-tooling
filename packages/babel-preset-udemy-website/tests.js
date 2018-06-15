96f4e5f19bcad12f399d2f1f4f8b06946c895dcc
JJ Behrens <jj.behrens@udemy.com>
2018-06-15 14:03:13 -0700

    Added --verbose and --update as options to the tests.js command

--- packages/babel-preset-udemy-website/tests.js
+++ packages/babel-preset-udemy-website/tests.js
@@ -1,10 +1,41 @@
 'use strict';
 
+/**
+ * Usage: node packages/babel-preset-udemy-website/tests.js [--verbose] [--update]
+ *
+ * --verbose - Print more debugging information.
+ * --update - Update the tests.
+ *
+ * Note, you have to run it from the base of the monorepo, otherwise the hashes that
+ * babel-plugin-react-css-modules generates will be different--go figure.
+ */
+
 const assert = require('assert');
 const fs = require('fs');
 const join = require('path').join;
 const babel = require('@babel/core');
 const preset = require('./index');
+
+const MODE = Object.freeze({
+    TEST: 'TEST',
+    UPDATE: 'UPDATE',
+});
+let mode = MODE.TEST;
+let verbose = false;
+
+process.argv.forEach(val => {
+    if (!val.startsWith('--')) {
+        // Skip "node" and "tests.js" if present.
+        return;
+    }
+    if (val === '--verbose') {
+        verbose = true;
+    } else if (val === '--update') {
+        mode = MODE.UPDATE;
+    } else {
+        throw new Error(`Unknown argument: ${val}`);
+    }
+});
 
 // eslint-disable-next-line no-console
 console.log('test babel-preset-udemy-website');
@@ -16,19 +47,18 @@ fs.readdirSync(join(__dirname, 'tests/source')).filter(name => name.endsWith('.j
     // eslint-disable-next-line no-console
     console.log(`\tcheck ${name}`);
 
-    // Uncomment this to debug the tests:
-    //
-    // /* eslint-disable no-console */
-    // console.log('===== expected =====');
-    // console.log(expected);
-    // console.log('===== result =====');
-    // console.log(result);
-    // /* eslint-enable no-console */
-    //
-    // If you just want to overwrite the result files and look at the diff, use the following, and
-    // get rid of the assertion:
-    //
-    // fs.writeFileSync(join(__dirname, 'tests/result', name), `${result}\n`, { encoding: 'UTF-8' });
+    if (verbose && result.trim() !== expected.trim()) {
+        /* eslint-disable no-console */
+        console.log('===== expected =====');
+        console.log(expected);
+        console.log('===== result =====');
+        console.log(result);
+        /* eslint-enable no-console */
+    }
 
-    assert.equal(result.trim(), expected.trim());
+    if (mode === MODE.TEST) {
+        assert.equal(result.trim(), expected.trim());
+    } else if (mode === MODE.UPDATE) {
+        fs.writeFileSync(join(__dirname, 'tests/result', name), `${result}\n`, { encoding: 'UTF-8' });
+    }
 });
