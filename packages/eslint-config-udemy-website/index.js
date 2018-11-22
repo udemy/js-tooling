177a98a02086546d0f4fd1f35abe5c4cae8dd75d
Cansin Yildiz <cansin@udemy.com>
2018-11-21 23:30:28 -0800

    feat: Disalllow angular as a global keyword.
    BREAKING CHANGE: We no longer allow "angular" global keyword. Users need to `import angular from "angular";` in order to use `angular`.

--- packages/eslint-config-udemy-website/index.js
+++ packages/eslint-config-udemy-website/index.js
@@ -122,7 +122,6 @@ module.exports = {
         UD: true,
         module: false,
         inject: false,
-        angular: false,
         gettext: false,
         ngettext: false,
         npgettext: false,
