677eb3ad14ccfce8bddfaf0f7e3597cfaead758a
Joe Cancilla <joe.cancilla@udemy.com>
2019-11-14 13:18:21 -0800

    [TF-3961] add eslint-plugin-react-hooks

---
+++ packages/eslint-config-udemy-react-addons/parts/react-hooks.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    plugins: ['react-hooks'],
+    rules: {
+        // Only call Hooks from react functions and only call Hooks at the top level
+        'react-hooks/rules-of-hooks': 'error',
+        // Verifies the list of dependencies for Hooks like useEffect and similar
+        'react-hooks/exhaustive-deps': 'warn',
+    },
+};
