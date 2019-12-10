6b4770a8c263242ea6214af02a127ac344707402
Joe Cancilla <joe.cancilla@udemy.com>
2019-12-10 13:05:52 -0800

    [TF-3961] make react-hooks/exhaustive-deps an error

--- packages/eslint-config-udemy-react-addons/parts/react-hooks.js
+++ packages/eslint-config-udemy-react-addons/parts/react-hooks.js
@@ -6,6 +6,6 @@ module.exports = {
         // Only call Hooks from react functions and only call Hooks at the top level
         'react-hooks/rules-of-hooks': 'error',
         // Verifies the list of dependencies for Hooks like useEffect and similar
-        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/exhaustive-deps': 'error',
     },
 };
